011854d07ee12d188aaa0820f1e1f091967f7cd1
Peter Mace <peterjmace@gmail.com>
2021-03-22 20:27:14 -0700

    created basic html setup/js

---
+++ app.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const scoreDisplay = document.querySelector('#money')
+  const startBtn = document.querySelector('#start-button')
+  const width = 10
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
