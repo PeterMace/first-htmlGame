f2286d57a989fb0a32f889ed72af9b3fdffbd01c
“Peter <peterjmace@gmail.com>
2022-04-20 22:01:49 -0400

    store functionality started

--- app.js
+++ app.js
@@ -5,25 +5,6 @@ import ShopItem from './shopItem.js';
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
-  //shop items
-  let shopItemList = 
-  [
-    new ShopItem('lemon', 48, 12.00),
-    new ShopItem('lemon', 24, 7.00),
-    new ShopItem('lemon', 12, 4.00),
-
-    new ShopItem('ice', 500, 10.00),
-    new ShopItem('ice', 200, 4.50),
-    new ShopItem('ice', 48, 2.75),
-
-    new ShopItem('sugar', 50, 14.50),
-    new ShopItem('sugar', 15, 5.25),
-    new ShopItem('sugar', 5, 2.00),
-
-    new ShopItem('cups', 200, 4.50),
-    new ShopItem('cups', 75, 2.75),
-  ]
-  
   function load_images(nameSet, maxImgNum){
     for(let i = 1; i <= maxImgNum; i++){
       const name = `${nameSet}_${i}`;
@@ -34,6 +15,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
       images[name] = element;
     }
 
+  }
+
+  function load_shop(shopItems){
+    shop.set_items(shopItems);
   }
 
   function draw(img, x, y){
@@ -62,6 +47,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
     //Spawn delay control
     horde.spawnDelay ? horde.spawnDelay-=1 : horde.spawnDelay;
+    
     //Zombie round duration
     game.roundTimer-=1;
     if(game.roundTimer < 0){
@@ -72,7 +58,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   //Game Display Intialization
   const canvas = document.getElementById('game');
+  const shopDiv = document.getElementById('shop');
   const ctx = canvas.getContext('2d');
+  //Set low Z index so stores displays in front
+  canvas.style.zIndex = "1";
   ctx.imageSmoothingEnabled = false;
 
   //Game Objects
@@ -81,20 +70,58 @@ window.addEventListener('DOMContentLoaded', (event) => {
   let horde = new Horde();
   const images = {}
 
-  //Game Images Loaded
+  //Game Objects Loading
   load_images("appear", 11);
   load_images("idle", 6);
   load_images("walk", 10);
   load_images("die", 8);
   load_images("drink", 7);
 
+  //Shop items
+  let shopItemList = 
+    [
+      new ShopItem('lemon', 48, 12.00),
+      new ShopItem('lemon', 24, 7.00),
+      new ShopItem('lemon', 12, 4.00),
   
-  let animation = window.setInterval(animate, 125);
+      new ShopItem('ice', 500, 10.00),
+      new ShopItem('ice', 200, 4.50),
+      new ShopItem('ice', 48, 2.75),
+  
+      new ShopItem('sugar', 50, 14.50),
+      new ShopItem('sugar', 15, 5.25),
+      new ShopItem('sugar', 5, 2.00),
+  
+      new ShopItem('cups', 200, 4.50),
+      new ShopItem('cups', 75, 2.75),
+    ];
+    
+
+
+  load_shop(shopItemList);
+
+  displayShop();
+  //let animation = window.setInterval(animate, 125);
 
   function clearAnimation(){
     clearInterval(animation);
     clearScreen();
   }
 
+  function displayShop(){
+    const shopHtml = shop.getShopDisplay();
+    console.log(shopHtml)
+    shopDiv.appendChild(shopHtml);
+  }
+
+  
+  function clearShop(){
+    const shopHtml = shop.getShopDisplay();
+    canvas.appendChild(shopHtml);
+  }
+
+  function startRound(){
+    
+  }
   
 })
--- shop.js
+++ shop.js
@@ -9,11 +9,18 @@ export default class Shop{
         this._shop = items
     }
 
+    get_items(){
+        return this._shop;
+    }
+
+
     getShopDisplay(){
         for (let item of this._shop){
             let displayButton = document.createElement('button');
             displayButton.setAttribute('id', `${item.type}:${item.amount}`)
+            displayButton.setAttribute('class', 'shop_button')
             displayButton.innerText = `${item.amount} ${item.type} \n $${item.price.toFixed(2)}`;
+            displayButton.style.zIndex = "2";
             this.UI.appendChild(displayButton);
         }
         return this.UI
